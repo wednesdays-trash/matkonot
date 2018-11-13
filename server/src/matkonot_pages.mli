310b38fac05cfb82bd79457ec54dd296bd3821d4
Maor Nadosh <qoev@protonmail.com>
2018-11-13 15:40:53 +0200

    :tulip:

--- server/src/matkonot_pages.mli
+++ server/src/matkonot_pages.mli
@@ -3,5 +3,3 @@ val search : string
 val error  : string
 
 val make_error_page : ('a, unit, string, string) format4 -> 'a
-
-
--- server/src/utils.ml
+++ server/src/utils.ml
@@ -1,13 +1,5 @@
 open Containers
 
-let all_true bools =
-    List.fold_left (fun a b -> if a && b then true else false) true bools
 
 let read_file name =
-    let ic = open_in name in
-    let n = in_channel_length ic in
-    let s = Bytes.create n in
-    really_input ic s 0 n;
-    close_in ic;
-    s |> Bytes.to_string
-
+  IO.(with_in name read_all)
